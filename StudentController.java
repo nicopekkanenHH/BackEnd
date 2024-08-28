bc1801da0fa039706d7bb3c446a19d3cb62cc609
nicopekkanenHH <165182826+nicopekkanenHH@users.noreply.github.com>
2024-08-28 19:15:08 +0300

    Add files via upload

---
+++ StudentController.java
@@ -0,0 +1,28 @@
+package syksy24.backend.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import syksy24.backend.domain.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/hello")
+    public String getStudentList(Model model) {
+        
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Nico", "Pekkanen"));
+        students.add(new Student("Joona", "Olenius"));
+        students.add(new Student("Elia", "Hiltunen"));
+
+       
+        model.addAttribute("students", students);
+
+        
+        return "studentlist";
+    }
+}
